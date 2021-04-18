a2e32516263bc9910037e7a305b0e09cae1fde5f
Marwa Ghassa <marwaghassa7@gmail.com>
2021-04-18 15:28:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@
 
 document.addEventListener("DOMContentLoaded",function(e){
   let par=document.getElementById('text');
- par.textcontent= "This is really cool!";
+ par.innerHTML= "This is really cool!";
 });
